9df548b6e8c4c7ebd7a1a4c91e15b95b40d3af86
Frikitrok <ZGrabovskiy@gmail.com>
2017-12-08 03:18:26 +0200

    small fix

--- docker_confluance.sh
+++ docker_confluance.sh
@@ -2,7 +2,6 @@ mkdir conf-volume \
 	&& mkdir conf-cont \
 	&& cd conf-cont \
 	&& cp /vagrant/Dockerfile . \
-	&& cp /vagrant/atlassian-confluence-6.5.2-x64.bin . \
 	&& cp /vagrant/response.varfile .
 docker build -t confluance	.
 docker run --rm --name confluance-machine -d -p 8080:8090 confluance
